15619b9e9bd8393ebedc951d021330a86388797a
Yuriy Kiselev <y.kiselev@SD-1690.local>
2022-01-15 00:48:50 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist'),
+    publicPath: path.resolve(__dirname, './assets'),
   },
   devServer: {
     contentBase: path.join(__dirname, './'),
